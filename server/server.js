ae52dc2f534b5402f8c5d23631cdda7a3b4c9c18
Chadi hoayeK <chadihoayek@Chadis-MacBook-Pro.local>
2024-07-09 19:39:48 -0700

    added more things to server.js, working on appollo server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const { ApolloServer } = require('@apollo/server');
 const app = express(); 
-
+const path = require('path');
 
 const port = process.env.PORT || 3000;
+
+const db = require('./config/connection');
 
 
 app.listen(port, () => {
