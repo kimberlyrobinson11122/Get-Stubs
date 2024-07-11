25aa17b73ae758479d2dfd1e13e13ce2248dec63
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-07-10 18:42:47 -0700

    added mutations for add user and add event

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,21 @@
+import { gql } from "@apollo/client";
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      id
+      username
+      email
+    }
+  }`;
+
+export const ADD_EVENT = gql`
+  mutation addEvent($title: String!, $description: String!, $date: String!, $location: String!) {
+    addEvent(title: $title, description: $description, date: $date, location: $location) {
+      id
+      title
+      description
+      date
+      location
+    }
+  }`;
